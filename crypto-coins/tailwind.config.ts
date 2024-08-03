2fd0ae9949ec004fa305e804bd9170cc4c9387ca
BASadityaSrinivas <basaditya007@yahoo.com>
2024-08-04 03:50:55 +0530

    add tailwind themes

--- crypto-coins/tailwind.config.ts
+++ crypto-coins/tailwind.config.ts
@@ -1,3 +1,4 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,6 +6,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/[object Object].js"
   ],
   theme: {
     extend: {
@@ -15,9 +17,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), nextui()],
   daisyui: {
-    themes: ["winter", "dark", "bumblebee"],
+    themes: ["winter", "dark", "bumblebee", "cyberpunk", "valentine"],
   },
 };
 export default config;
