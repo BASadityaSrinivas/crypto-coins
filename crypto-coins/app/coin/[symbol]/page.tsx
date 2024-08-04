5b0f8e4339b47315ab074b99e594ce3ec1394f39
BASadityaSrinivas <basaditya007@yahoo.com>
2024-08-04 18:11:37 +0530

    Fetch and Display Coins data

--- crypto-coins/app/coin/[symbol]/page.tsx
+++ crypto-coins/app/coin/[symbol]/page.tsx
@@ -1,46 +1,79 @@
 'use client';
 import PriceTable from '@/app/components/PriceTable';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import React from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter, useParams } from 'next/navigation';
 
 interface Coin {
-  symbol: string;
-  name: string;
+  coin_id: string;
+  coin_symbol: string;
+  coin_name: string;
   subreddit: string;
+  image: string;
 }
 
-const coinsInfo = [{"symbol": "btc", "name": "Bitcoin", "subreddit": "https://www.reddit.com/r/Bitcoin/"}];
-
-
-export default function CoinData({params}: any) {
-
-  const coinsData: Coin[] = coinsInfo;
-
+export default function CoinData() {
+  const params = useParams<{ symbol: string }>();
+  const [coinData, setCoinData] = useState<Coin | null>(null);
   const router = useRouter();
+
+  console.log("Params: ", params);
+
+  useEffect(() => {
+    const fetchCoinsData = async () => {
+      if (!params.symbol) {
+        console.error('No symbol provided');
+        return;
+      }
+      
+      try {
+        const response = await fetch('/api/coins');
+        if (response.ok) {
+          const data: Coin[] = await response.json();
+          const coin = data.find(coin => coin.coin_id === params.symbol);
+          console.log("coin ", coin)
+          setCoinData(coin || null);
+        } else {
+          console.error('Failed to fetch coins data');
+        }
+      } catch (error) {
+        console.error('Error fetching coins data:', error);
+      }
+    };
+
+    fetchCoinsData();
+  }, [params.symbol]);
+
+  if (!coinData) {
+    return <div>Loading...</div>;
+  }
+
   const handleOnClick = () => {
     router.push('/');
-  }  
+  };
 
   return (
     <div>
-      {coinsData.map(coin => 
-        <div>
-          <button className='rounded-md ml-20 mt-16 pl-6 pr-8 py-2 bg-base-300' onClick={handleOnClick}>
-            <img src="/assets/chevron-left.png" alt="Logo" height={30} width={30} />
-          </button>
-          <h1 className='text-3xl font-bold ml-5 px-16 mt-5'>{coin.symbol.toUpperCase()}</h1>
-          <h2 className='text-large font-bold ml-10 mt-5 px-16 text-[#1f7e7e]'>{coin.name}</h2>
-          <div className='flex flex-wrap-reverse ml-10 px-16 pr-52 mt-2'>
-            <p className='text-medium font-semibold'>SubReddit: </p>
-            <div className='flex-auto w-64 grow-0 shrink'> 
-            <Link href={coin.subreddit}> 
-                <img className='ml-10' src="/assets/reddit.png" alt="reddit-png" height={20} width={20}/>
+      <div key={coinData.coin_symbol}>
+        <button className='rounded-md ml-20 mt-16 pl-6 pr-8 py-2 bg-base-300' onClick={handleOnClick}>
+          <img src="/assets/chevron-left.png" alt="Logo" height={30} width={30} />
+        </button>
+        <div className='flex ml-5 mt-5'>
+          <h1 className='text-3xl font-bold mt-5 ml-20'>{coinData.coin_symbol.toUpperCase()}</h1>
+          <img className='ml-5 mt-2' src={coinData.image} alt='coin-img' height={20} width={60}></img>
+        </div>
+        
+        <h2 className='text-large font-bold ml-10 mt-5 px-16 text-[#1f7e7e]'>{coinData.coin_name}</h2>
+        <div className='flex flex-wrap-reverse ml-10 px-16 pr-52 mt-2'>
+          <p className='text-medium font-semibold'>SubReddit: </p>
+          <div className='flex-auto w-64 grow-0 shrink'> 
+            <Link href={coinData.subreddit} target="_blank"> 
+              <img className='ml-10' src="/assets/reddit.png" alt="reddit-png" height={20} width={20}/>
             </Link>
-              </div>
           </div>
         </div>
-      )}
-      <PriceTable/>
+      </div>
+      <PriceTable coinId={coinData.coin_id} />
     </div>
-  )}
+  );
+}
--- crypto-coins/app/components/CryptoTable.tsx
+++ crypto-coins/app/components/CryptoTable.tsx
@@ -1,57 +1,75 @@
 'use client';
+
 import Link from 'next/link';
-import React from 'react'
-import { useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface Coin {
-  symbol: string;
-  name: string;
+  coin_id: string
+  coin_symbol: string;
+  coin_name: string;
   subreddit: string;
+  image: string;
 }
 
-const coinsInfo = [{"symbol": "btc", "name": "Bitcoin", "subreddit": "https://www.reddit.com/r/Bitcoin/"},
-                   {"symbol": "eth", "name": "Ethereum", "subreddit": "https://www.reddit.com/r/ethereum/"}];
-
 export const CryptoTable = () => {
-  const coinsData: Coin[] = coinsInfo;
-  
+  const [coinsData, setCoinsData] = useState<Coin[]>([]);
   const router = useRouter();
+
+  useEffect(() => {
+    const fetchCoinsData = async () => {
+      try {
+        const response = await fetch('/api/coins');
+        console.log("Fetching the coins data")
+        if (response.ok) {
+          const data: Coin[] = await response.json();
+          setCoinsData(data);
+        } else {
+          console.error('Failed to fetch coins data');
+        }
+      } catch (error) {
+        console.error('Error fetching coins data:', error);
+      }
+    };
+
+    fetchCoinsData();
+  }, []);
+
   const handleRowClick = (row: Coin) => {
-    router.push(`/coin/${row.symbol}`);
-  }  
+    router.push(`/coin/${row.coin_id}`);
+  };
 
   return (
     <div className="flex">
-    <table className='table m-20 p-10 table-pin-rows'>
-      <thead>
-        <tr>
-          <th>Symbol</th>
-          <th>Name</th>
-          <th>Reddit</th>
-        </tr>
-      </thead>
-      <tbody>
-      {coinsData.map(coin => 
-        <tr className="hover:bg-base-300 hover:cursor-pointer" onClick={() => {handleRowClick(coin)}}>
-          <td>{coin.symbol.toUpperCase()}</td>
-          <td>{coin.name}</td>
-          <td>
-            <Link href={coin.subreddit}> 
-              <img className='m-auto'  src="assets/reddit.png" alt='reddit-png' height={20} width={20}/>
-            </Link>
-          </td>
-        </tr>
-    )}
-        {/* <tr className="hover">
-          <td>BTC</td>
-          <td>Bitcoin</td>
-        </tr>
-        <tr className="hover">
-          <td>ETH</td>
-          <td>Ethereum</td>
-        </tr> */}
-      </tbody>
-    </table>
+      <table className='table m-20 p-10 table-pin-rows'>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Reddit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coinsData.map(coin => (coin.coin_id ?
+            <tr key={coin.coin_id} className="hover:bg-base-300 hover:cursor-pointer" onClick={() => handleRowClick(coin)}>
+              <td> 
+                <div className='flex place-content-center'>
+                  <p>{coin.coin_symbol.toUpperCase()}</p>
+                  <img className='ml-5' src={coin.image} alt='coin-img' height={20} width={20}></img>
+                </div>
+              </td>
+              <td className='font-bold'>{coin.coin_name}</td>
+              <td>
+                <Link href={coin.subreddit} target="_blank">
+                  <img className='m-auto' src="/assets/reddit.png" alt='reddit-png' height={20} width={20} />
+                </Link>
+              </td>
+            </tr>
+            : 
+            <p></p>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
